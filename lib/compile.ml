5be790b57a81c804daaa9bbea16a9d8e6982cecc
yhs0602 <jourhyang123@gmail.com>
2024-06-01 17:41:55 +0900

    :sparkles: Graph union

--- lib/compile.ml
+++ lib/compile.ml
@@ -35,6 +35,22 @@ module Graph = struct
       det_map = Map.empty (module Id);
       obs_map = Map.empty (module Id);
     }
+  
+  let union g1 g2 = 
+    {
+      vertices = g1.vertices @ g2.vertices;
+      arcs = g1.arcs @ g2.arcs;
+      det_map = Map.merge g1.det_map g2.det_map ~f:(fun ~key:_ v -> 
+        match v with
+        | `Left det | `Right det -> Some det
+        | `Both _ -> failwith "Graph.union: duplicate deterministic expression"
+      );
+      obs_map = Map.merge g1.obs_map g2.obs_map ~f:(fun ~key:_ v -> 
+        match v with
+        | `Left obs | `Right obs -> Some obs
+        | `Both _ -> failwith "Graph.union: duplicate observation"
+      );
+    }
 end
 
 let gen_sym =
