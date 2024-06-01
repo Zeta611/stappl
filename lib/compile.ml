34dfdee598b05381aeff6e9e662360f2d158c300
Jay Lee <jaeho.lee@snu.ac.kr>
2024-06-01 17:30:59 +0900

    :sparkles: Implement substitution

--- lib/compile.ml
+++ lib/compile.ml
@@ -1,27 +1,25 @@
 open Core
+open Program
 
 type number = Int of int | Float of float
 
 module Env = struct
-  type t = (Program.id, Program.fn, String.comparator_witness) Map.t
+  type t = (Id.t, fn, Id.comparator_witness) Map.t
 
-  let empty : t = Map.empty (module String)
-
-  let add (env : t) ~(name : Program.id) ~(fn : Program.fn) =
-    Map.add env ~key:name ~data:fn
-
-  let find_exn (env : t) ~(name : Program.id) : Program.fn =
-    Map.find_exn env name
+  let empty : t = Map.empty (module Id)
+  let add (env : t) ~(name : Id.t) ~(fn : fn) = Map.add env ~key:name ~data:fn
+  let find_exn (env : t) ~(name : Id.t) : fn = Map.find_exn env name
 end
 
-module Graphical_model = struct
-  type vertex = Program.id
+module Pred = struct
+  type t
+end
+
+module Graph = struct
+  type vertex = Id.t
   type arc = vertex * vertex
-
-  type det_map =
-    (Program.id, Program.Det_exp.t, String.comparator_witness) Map.t
-
-  type obs_map = (Program.id, number, String.comparator_witness) Map.t
+  type det_map = (Id.t, Det_exp.t, Id.comparator_witness) Map.t
+  type obs_map = (Id.t, number, Id.comparator_witness) Map.t
 
   type t = {
     vertices : vertex list;
@@ -34,13 +32,57 @@ module Graphical_model = struct
     {
       vertices = [];
       arcs = [];
-      det_map = Map.empty (module String);
-      obs_map = Map.empty (module String);
+      det_map = Map.empty (module Id);
+      obs_map = Map.empty (module Id);
     }
 end
 
-let compile (env : Env.t) (exp : Program.Exp.t) :
-    Graphical_model.t * Program.Det_exp.t =
+let gen_sym =
+  let cnt = ref 0 in
+  fun () ->
+    incr cnt;
+    Printf.sprintf "X_%d" !cnt
+
+let rec sub (exp : Exp.t) (x : Id.t) (det_exp : Det_exp.t) : Exp.t =
+  let sub' exp = sub exp x det_exp in
   match exp with
-  | Int n -> (Graphical_model.empty, Program.Det_exp.Int n)
+  | Int n -> Int n
+  | Real r -> Real r
+  | Var y when Id.(x = y) -> Det_exp.to_exp det_exp
+  | Var y -> Var y
+  | Add (e1, e2) -> Add (sub' e1, sub' e2)
+  | Radd (e1, e2) -> Radd (sub' e1, sub' e2)
+  | Minus (e1, e2) -> Minus (sub' e1, sub' e2)
+  | Rminus (e1, e2) -> Rminus (sub' e1, sub' e2)
+  | Neg e -> Neg (sub' e)
+  | Rneg e -> Rneg (sub' e)
+  | Mult (e1, e2) -> Mult (sub' e1, sub' e2)
+  | Rmult (e1, e2) -> Rmult (sub' e1, sub' e2)
+  | Div (e1, e2) -> Div (sub' e1, sub' e2)
+  | Rdiv (e1, e2) -> Rdiv (sub' e1, sub' e2)
+  | Eq (e1, e2) -> Eq (sub' e1, sub' e2)
+  | Noteq (e1, e2) -> Noteq (sub' e1, sub' e2)
+  | Less (e1, e2) -> Less (sub' e1, sub' e2)
+  | And (e1, e2) -> And (sub' e1, sub' e2)
+  | Or (e1, e2) -> Or (sub' e1, sub' e2)
+  | Seq (e1, e2) -> Seq (sub' e1, sub' e2)
+  | Not e -> Not (sub' e)
+  | List es -> List (List.map es ~f:sub')
+  | Record fs -> Record (List.map fs ~f:(fun (f, e) -> (f, sub' e)))
+  | Assign (y, e, body) when Id.(x = y) -> Assign (y, sub' e, body)
+  | Assign (y, e, body) when not (Set.mem (Det_exp.fv det_exp) y) ->
+      Assign (y, sub' e, sub' body)
+  | Assign (y, e, body) ->
+      let z = gen_sym () in
+      Assign (z, sub' e, sub' @@ sub body y (Det_exp.Var z))
+  | If (e_pred, e_con, e_alt) -> If (sub' e_pred, sub' e_con, sub' e_alt)
+  | Call (f, es) -> Call (f, List.map es ~f:sub')
+  | Sample e -> Sample (sub' e)
+  | Observe (e1, e2) -> Observe (sub' e1, sub' e2)
+
+let compile (env : Env.t) (pred : Pred.t) (exp : Exp.t) : Graph.t * Det_exp.t =
+  ignore env;
+  ignore pred;
+  match exp with
+  | Int n -> (Graph.empty, Det_exp.Int n)
   | _ -> failwith "Not implemented"
--- lib/program.ml
+++ lib/program.ml
@@ -1,12 +1,11 @@
 open Core
-
-type id = string [@@deriving sexp]
+module Id = String
 
 module Exp = struct
   type t =
     | Int of int
     | Real of float
-    | Var of id
+    | Var of Id.t
     | Add of t * t
     | Radd of t * t
     | Minus of t * t
@@ -26,22 +25,22 @@ module Exp = struct
     | Not of t
     | List of t list
     | Record of (t * t) list
-    | Assign of id * t * t
+    | Assign of Id.t * t * t
     | If of t * t * t
-    | Call of id * t list
+    | Call of Id.t * t list
     | Sample of t
     | Observe of t * t
   [@@deriving sexp]
 end
 
-type fn = { name : id; params : id list; body : Exp.t } [@@deriving sexp]
+type fn = { name : Id.t; params : Id.t list; body : Exp.t } [@@deriving sexp]
 type program = { funs : fn list; exp : Exp.t } [@@deriving sexp]
 
 module Det_exp = struct
   type t =
     | Int of int
     | Real of float
-    | Var of id
+    | Var of Id.t
     | Add of t * t
     | Radd of t * t
     | Minus of t * t
@@ -61,8 +60,66 @@ module Det_exp = struct
     | List of t list
     | Record of (t * t) list
     | If of t * t * t
-    | Prim_call of id * t list
+    | Prim_call of Id.t * t list
   [@@deriving sexp]
+
+  let rec to_exp = function
+    | Int i -> Exp.Int i
+    | Real f -> Exp.Real f
+    | Var x -> Exp.Var x
+    | Add (e1, e2) -> Exp.Add (to_exp e1, to_exp e2)
+    | Radd (e1, e2) -> Exp.Radd (to_exp e1, to_exp e2)
+    | Minus (e1, e2) -> Exp.Minus (to_exp e1, to_exp e2)
+    | Rminus (e1, e2) -> Exp.Rminus (to_exp e1, to_exp e2)
+    | Neg e -> Exp.Neg (to_exp e)
+    | Rneg e -> Exp.Rneg (to_exp e)
+    | Mult (e1, e2) -> Exp.Mult (to_exp e1, to_exp e2)
+    | Rmult (e1, e2) -> Exp.Rmult (to_exp e1, to_exp e2)
+    | Div (e1, e2) -> Exp.Div (to_exp e1, to_exp e2)
+    | Rdiv (e1, e2) -> Exp.Rdiv (to_exp e1, to_exp e2)
+    | Eq (e1, e2) -> Exp.Eq (to_exp e1, to_exp e2)
+    | Noteq (e1, e2) -> Exp.Noteq (to_exp e1, to_exp e2)
+    | Less (e1, e2) -> Exp.Less (to_exp e1, to_exp e2)
+    | And (e1, e2) -> Exp.And (to_exp e1, to_exp e2)
+    | Or (e1, e2) -> Exp.Or (to_exp e1, to_exp e2)
+    | Not e -> Exp.Not (to_exp e)
+    | List es -> Exp.List (List.map ~f:to_exp es)
+    | Record fields ->
+        Exp.Record (List.map ~f:(fun (k, v) -> (to_exp k, to_exp v)) fields)
+    | If (cond, e1, e2) -> Exp.If (to_exp cond, to_exp e1, to_exp e2)
+    | Prim_call (id, es) -> Exp.Call (id, List.map ~f:to_exp es)
+
+  let rec fv : t -> (Id.t, Id.comparator_witness) Set.t = function
+    | Int _ | Real _ -> Set.empty (module Id)
+    | Var x -> Set.singleton (module Id) x
+    | Add (e1, e2)
+    | Radd (e1, e2)
+    | Minus (e1, e2)
+    | Rminus (e1, e2)
+    | Mult (e1, e2)
+    | Rmult (e1, e2)
+    | Div (e1, e2)
+    | Rdiv (e1, e2)
+    | Eq (e1, e2)
+    | Noteq (e1, e2)
+    | Less (e1, e2)
+    | And (e1, e2)
+    | Or (e1, e2) ->
+        Set.union (fv e1) (fv e2)
+    | Neg e | Rneg e | Not e -> fv e
+    | List es ->
+        List.fold es
+          ~init:(Set.empty (module Id))
+          ~f:(fun acc e -> Set.union acc (fv e))
+    | Record fields ->
+        List.fold fields
+          ~init:(Set.empty (module Id))
+          ~f:(fun acc (k, v) -> Set.(union acc (union (fv k) (fv v))))
+    | If (cond, e1, e2) -> Set.(union (fv cond) (union (fv e1) (fv e2)))
+    | Prim_call (id, es) ->
+        List.fold es
+          ~init:(Set.singleton (module Id) id)
+          ~f:(fun acc e -> Set.union acc (fv e))
 end
 
 let pp pgm = print_endline (sexp_of_program pgm |> Sexp.to_string_hum)
